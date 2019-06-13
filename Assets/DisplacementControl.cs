d7c6a9af2861c354f9876d1ef198569606b8ea2e
emr <renquin@awtce.be>
2019-06-13 09:00:35 +0200

    Initial commit / tuto completed

---
+++ Assets/DisplacementControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplacementControl : MonoBehaviour
+{
+    public float displacementAmount;
+    //public ParticleSystem explosionParticles;
+    MeshRenderer meshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displacementAmount = Mathf.Lerp(displacementAmount, 0, Time.deltaTime);
+        meshRenderer.material.SetFloat("_Amount", displacementAmount);
+
+        if (Input.GetButtonDown("Jump")) {
+            displacementAmount += 1.0f;
+            //explosionParticles.Play();
+        }
+    }
+}
